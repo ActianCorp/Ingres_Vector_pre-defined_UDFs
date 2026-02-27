bab300162c58b088bed1059b4493f4427f3675b7
Gabriel Lebis <gabriel.lebis@actian.com>
2026-02-27 10:41:33 +0100

    one_hot_encode: fix header comment and input parameter declaration format (no functional change)

--- ml_preprocessing/sql/one_hot_encode.sql
+++ ml_preprocessing/sql/one_hot_encode.sql
@@ -1,5 +1,7 @@
--- one_hot_encode(val, distinct_count) - Encode categorical features as a one-hot numeric array. To be called as VECTOR(one_hot_encode(val), <distinct_count>, INT4) where distinct_count is a literal number determined by running query SELECT COUNT(DISTINCT val)
-CREATE FUNCTION one_hot_encode(val INT)
+-- one_hot_encode(val) - Encode categorical features as a one-hot numeric array. To be called as VECTOR(one_hot_encode(val), <distinct_count>, INT4) where distinct_count is a literal number determined by running query SELECT COUNT(DISTINCT val)
+CREATE FUNCTION one_hot_encode(
+    val INT
+)
 RETURN (VARCHAR(32000))
 AS
 DECLARE
@@ -11,7 +13,9 @@ BEGIN
     RETURN CONCAT('[', REPEAT('0,', rank-1), '1', REPEAT(',0', distinct_count-rank), ']');
 END;\g
 
-CREATE FUNCTION one_hot_encode(val FLOAT8)
+CREATE FUNCTION one_hot_encode(
+    val FLOAT8
+)
 RETURN (VARCHAR(32000))
 AS
 DECLARE
@@ -23,7 +27,9 @@ BEGIN
     RETURN CONCAT('[', REPEAT('0,', rank-1), '1', REPEAT(',0', distinct_count-rank), ']');
 END;\g
 
-CREATE FUNCTION one_hot_encode(val VARCHAR(32000))
+CREATE FUNCTION one_hot_encode(
+    val VARCHAR(32000)
+)
 RETURN (VARCHAR(32000))
 AS
 DECLARE
